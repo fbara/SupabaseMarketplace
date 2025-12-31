d66a8d37c3dddceeac68ee2749ec6c92a560d16b
fbara <fbara1@yahoo.com>
2025-12-31 12:18:46 -0500

    Initial Commit

---
+++ SupabaseMarketplace/SupabaseMarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseMarketplaceApp.swift
+//  SupabaseMarketplace
+//
+//  Created by Frank Bara on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseMarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
